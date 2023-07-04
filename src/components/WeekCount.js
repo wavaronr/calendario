f862000527d9575d0085c074f18e203920b54175
wavaronr <alexney1@hotmail.com>
2023-07-03 21:32:10 -0500

    create-offCanvas
    create-offCanvas in weekcount

--- src/components/WeekCount.js
+++ src/components/WeekCount.js
@@ -1,4 +1,5 @@
 import { getISOWeek } from "date-fns";
+import AsesorOffCanvas from "./asesorOffCanvas";
 
 function WeekCount({ monthCalendario, yearSet }) {
   const day = [1, 8, 15, 22, 28];
@@ -8,15 +9,28 @@ function WeekCount({ monthCalendario, yearSet }) {
     return weekNumber;
   });
 
+   
+
   return (
+    <>
+    <AsesorOffCanvas/>
     <ol className="numerWeek">
       <li className="numerW-title">Sem</li>
       {weekNumbers.map((weekNumber) => (
         <li className="numerW" key={weekNumber.toString}>
-          {weekNumber}
+          <button
+            class="btn numerW"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasWithBothOptions"
+            aria-controls="offcanvasWithBothOptions"
+          >
+            {weekNumber}
+          </button>
         </li>
       ))}
     </ol>
+    </>
   );
 }
 
---
+++ src/components/asesorOffCanvas.js
@@ -0,0 +1,27 @@
+function asesorOffCanvas() {
+  return (
+    <div
+      class="offcanvas offcanvas-start"
+      data-bs-scroll="true"
+      tabindex="-1"
+      id="offcanvasWithBothOptions"
+      aria-labelledby="offcanvasWithBothOptionsLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+          Titlte
+        </h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">
+        <p>profiles </p>
+      </div>
+    </div>
+  );
+}
+export default asesorOffCanvas;
