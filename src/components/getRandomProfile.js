14fa7717cf9ca28f796af792ca6bbd1f1dc1c48e
wavaronr <alexney1@hotmail.com>
2023-08-04 21:46:34 -0500

    add getDataAsesor

--- src/components/getRandomProfile.js
+++ src/components/getRandomProfile.js
@@ -1,32 +1,40 @@
-// import dataAsesor from "../data/dataAsesor.json";
+// import dataAsesor from "../data/dataAsesor.json";// temporal
 import { getDataAsesor } from "./getDataAsesor";
 
 async function getRandomProfile() {
   const randomUsers = [];
 
+  const urlGet = "allProfiles"
   try {
-    const userApi = await getDataAsesor(1);
+    const userApi= await getDataAsesor(urlGet);
 
-    const users = userApi;
+    // const userApi=dataAsesor // temporal
 
     const availableIndexes = Array.from(
-      { length: users.length },
+      { length: userApi.length },
       (_, index) => index
     );
 
-    for (let i = 0; i < 4; i++) {
-      const randomIndex = Math.floor(Math.random() * availableIndexes.length);
-      const user = users[availableIndexes[randomIndex]];
-      randomUsers.push(user);
-      availableIndexes.splice(randomIndex, 1);
+    for (let j = 0; j < 52; j++) {
+      const currentAvailableIndexes = [...availableIndexes];
+      const usuariosAleatorios = [];
+    
+      for (let i = 0; i < 4; i++) {
+        const randomIndex = Math.floor(Math.random() * currentAvailableIndexes.length);
+        const user = userApi[currentAvailableIndexes[randomIndex]];
+        usuariosAleatorios.push(user);
+        currentAvailableIndexes.splice(randomIndex, 1);
+      }
+    
+      randomUsers.push(usuariosAleatorios);
     }
-
-    return randomUsers;
+        return randomUsers;
+    
   } catch (error) {
-    // Manejo de errores
     console.error(error);
     return [];
   }
+  
 }
 
 export default getRandomProfile;
