ea08b4c760c98bf07b0349d7ac8a6f08aad3801b
wavaronr <alexney1@hotmail.com>
2023-08-30 22:56:30 -0500

    Add content CardProfile

--- src/components/CardProfile.js
+++ src/components/CardProfile.js
@@ -1,8 +1,40 @@
+import { getDataProfile } from './getDataAsesor';
+import React, { useState, useEffect } from 'react';
+import '../css/calendario.css';
+
 function CardProfile() {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const profile = await getDataProfile();
+      setProfiles(profile);
+    };
+
+    fetchData();
+  }, []);
+
+  const cardsProfiles = profiles?.map(({ id, name, cargo }) => (
+    <div className="card" key={'card' + { id }}>
+      <div className="card-body" key={'card-body' + { id }}>
+        <h5 className="card-title" key={'card-title' + { id }}>
+          {name}
+        </h5>
+        <p class="card-text" key={'card-title' + { id }}>
+          {cargo}
+        </p>
+        <button href="#" class="btn btn-primary">
+          Editar
+        </button>
+      </div>
+    </div>
+  ));
 
   return (
     <>
-    profiles
+      <div className="container">
+        <div className="card-profiles">{cardsProfiles}</div>
+      </div>
     </>
   );
 }
